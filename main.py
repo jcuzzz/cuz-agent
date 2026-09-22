865c426c0acaad68522bc8fa8c830a879740091e
jcuz <jcuz@jcuz.com>
2026-09-22 20:39:05 +1000

    agent responds to hardcoded questions

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from cuz-agent!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
 
 
-if __name__ == "__main__":
-    main()
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+)
+
+
+print(response.choices[0].message.content)
